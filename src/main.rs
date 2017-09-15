a569361a1471f097f09b5d9f4cc2e9b95e0697d9
orangebaron <orangebaronroblox@gmail.com>
2017-09-14 20:14:49 -0400

    copypasted website example code

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
-mod abc;
-use abc::Traitb;
-pub fn dostuff<T: abc::Traitb>(asdf: &mut T) {
-	asdf.geta().print();
-	asdf.geta().seta(65);
-	asdf.geta().print();
-	asdf.setb(10);
+extern crate hyper;
+extern crate futures;
+use hyper::header::ContentLength;
+use hyper::server::{Http, Request, Response, Service};
+const PHRASE: &'static str = "Hello, World!";
+struct HelloWorld;
+impl Service for HelloWorld {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = futures::future::FutureResult<Self::Response, Self::Error>;
+
+    fn call(&self, _req: Request) -> Self::Future {
+        futures::future::ok(
+            Response::new()
+                .with_header(ContentLength(PHRASE.len() as u64))
+                .with_body(PHRASE)
+        )
+    }
 }
-#[allow(unused_variables)]
 fn main() {
-	let mut asdf = abc::maketraitb();
-	dostuff(&mut asdf);
-    let mut b = asdf.c();
-    *b = 66;
+    let addr = "127.0.0.1:3000".parse().unwrap();
+    let server = Http::new().bind(&addr, || Ok(HelloWorld)).unwrap();
+    server.run().unwrap();
 }
